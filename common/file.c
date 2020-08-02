eb32a88f75c9c150bc01765b398f31888ebf23b2
Alexandre Quesnel <131881+aquesnel@users.noreply.github.com>
2020-08-02 16:38:58 +0000

    RevertLOG migration in file.c to fix  writing to log file before logs are initialized

--- common/file.c
+++ common/file.c
@@ -277,7 +277,7 @@ l_file_read_section(int fd, int max_file_size, const char *section,
             {
                 if (g_strcasecmp(section, text) == 0)
                 {
-                    LOG_DEVEL(LOG_LEVEL_TRACE, "l_file_read_section - found section: %s", text);
+                    g_writeln("l_file_read_section - found section: %s", text);
                     while (file_read_ini_line(s, text,
                                               FILE_MAX_LINE_BYTES) == 0)
                     {
@@ -298,18 +298,18 @@ l_file_read_section(int fd, int max_file_size, const char *section,
                                 if (lvalue != 0)
                                 {
                                     list_add_item(values, (tbus)g_strdup(lvalue));
-                                    LOG_DEVEL(LOG_LEVEL_TRACE, "l_file_read_section - found line: %s = %s", name, lvalue);
+                                    g_writeln("l_file_read_section - found line: %s = %s", name, lvalue);
                                 }
                                 else
                                 {
                                     list_add_item(values, (tbus)g_strdup(""));
-                                    LOG_DEVEL(LOG_LEVEL_TRACE, "l_file_read_section - found line: %s = %s", name, "''");
+                                    g_writeln("l_file_read_section - found line: %s = %s", name, "''");
                                 }
                             }
                             else
                             {
                                 list_add_item(values, (tbus)g_strdup(value));
-                                LOG_DEVEL(LOG_LEVEL_TRACE, "l_file_read_section - found line: %s = %s", name, value);
+                                g_writeln("l_file_read_section - found line: %s = %s", name, value);
                             }
                         }
                     }
